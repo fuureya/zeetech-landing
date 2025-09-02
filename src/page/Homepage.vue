f17661e7eeb137d2668a210f7d7710aa7ecc7536
fuureya <agiljibrin009@gmail.com>
2025-09-02 13:48:58 +0800

    chore: first init

---
+++ src/page/Homepage.vue
@@ -0,0 +1,1 @@
+<template>anjay</template>
---
+++ src/router/index.js
@@ -0,0 +1,9 @@
+import Homepage from "@/page/Homepage.vue";
+import { createRouter, createWebHistory } from "vue-router";
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [{ path: "/", component: Homepage }],
+});
+
+export default router;
